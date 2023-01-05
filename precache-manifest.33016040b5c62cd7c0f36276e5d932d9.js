13d6d2782207f9d391d6169c6024b936bbc6e8d1
André Santa Cruz <shakcross@hotmail.com>
2023-01-05 01:44:21 -0500

    Updates

---
+++ precache-manifest.33016040b5c62cd7c0f36276e5d932d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a01224cfb5529a3aa",
+    "url": "/react_crash_course_globant/static/css/main.9af2ee8c.chunk.css"
+  },
+  {
+    "revision": "e96a01224cfb5529a3aa",
+    "url": "/react_crash_course_globant/static/js/main.e96a0122.chunk.js"
+  },
+  {
+    "revision": "a6b0c181771552b73dbd",
+    "url": "/react_crash_course_globant/static/js/1.a6b0c181.chunk.js"
+  },
+  {
+    "revision": "9139f9d54df420685779",
+    "url": "/react_crash_course_globant/static/js/2.9139f9d5.chunk.js"
+  },
+  {
+    "revision": "5084487bce61ff7624cd",
+    "url": "/react_crash_course_globant/static/js/runtime~main.5084487b.js"
+  },
+  {
+    "revision": "ea647b7bda514fa262db6c0e73d8fcd9",
+    "url": "/react_crash_course_globant/index.html"
+  }
+];
